753481b644164dba1f939975acc0d93e3bb54d00
Alex Votry <alexvotry@gmail.com>
2022-12-13 11:17:28 -0800

    add json-server in client

--- starter/apiPics/components/ImageShow.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import './styles.css';
-
-export default function ImageShow({src, alt}) {
-
-  return (
-      <img src={src} alt={alt} />
-  );
-}
--- starter/apiPics/secrets.js
+++
@@ -1,7 +0,0 @@
-export const AccessKey = "Client-ID EEt3LJ7oXvooaKtcZ_XV-XQqNoKf5EU-gmEDL6tVee4";
-export const root = "https://api.unsplash.com/search/photos";
-const SecretKey = "MGChqtu0vRYe1jqlJYeZYCAv8g8EARlW11dT6xl9hlI";
-
-
-// Authorization: Client-ID YOUR_ACCESS_KEY
-// GET /search/photos
