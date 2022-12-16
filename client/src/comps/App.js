16cdb789f9c72349929f40d0009c42662541d86f
Alex Votry <alexvotry@gmail.com>
2022-12-16 10:36:19 -0800

    made buttonPage to clear out the App component

--- client/src/comps/App.js
+++ client/src/comps/App.js
@@ -1,32 +1,11 @@
 import React from 'react';
-import Button from './components/Button';
-import {GoBell, GoCloudDownload, GoAlert, GoCalendar} from 'react-icons/go';
+import ButtonPage from './pages/ButtonPage';
 
 export default function App () {
-  const handleClick = () => {
-    console.log('click');
-  }
 
   return (
     <>
-      <div>
-        <Button danger outline onClick={handleClick} className="mb-5"><GoBell /> Click Me!!</Button>
-      </div>
-      <div>
-        <Button success rounded><GoAlert />second</Button>
-      </div>
-      <div>
-        <Button primary rounded outline><GoCloudDownload />third</Button>
-      </div>
-      <div>
-        <Button secondary outline><GoCalendar />forth</Button>
-      </div>
-      <div>
-        <Button warning rounded outline>fifth</Button>
-      </div>
-      <div>
-        <Button danger>sixth</Button>
-      </div>
+      <ButtonPage />
     </>
   );
 }
