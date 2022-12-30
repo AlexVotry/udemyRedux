f8ffae38aa8d40d08f97404648d9c88762dc8b0b
Alex Votry <alexvotry@gmail.com>
2022-12-30 15:36:44 -0800

    local state everywhere

--- client/src/media/components/UsersList.js
+++ client/src/media/components/UsersList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers, addUser } from '../store';
 import Skeleton from './Skeleton';
@@ -6,7 +6,11 @@ import Button from './Button';
 
 export default function UsersList () {
   const dispatch = useDispatch();
-  const { isLoading, data, error } =  useSelector(state => state.users);
+  const [isLoadingAllUsers, setIsLoadingAllUsers] = useState(false);
+  const [isLoadingUsersError, setIsLoadingUsersError] = useState(null);
+  const [isCreatingUser, setIsCreatingUser] = useState(false);
+  const [isCreatingUserError, setIsCreatingUserError] = useState(false);
+  const { data } =  useSelector(state => state.users);
 
   
   const renderedUsers = data.map(({name, id}) => {
@@ -20,21 +24,34 @@ export default function UsersList () {
   })
   
   useEffect(() => {
-    dispatch(fetchUsers());
+    setIsLoadingAllUsers(true);
+    dispatch(fetchUsers())
+      .unwrap()
+      .catch((err) => setIsLoadingUsersError(err))
+      .finally(() => setIsLoadingAllUsers(false));
   }, []);
 
-  const handleUserAdd = () => dispatch(addUser());
+  const handleUserAdd = () => {
+    setIsCreatingUser(true);
+    dispatch(addUser())
+      .unwrap()
+      .catch((err) => setIsCreatingUserError(err))
+      .finally(() => setIsCreatingUser(false));
+  };
 
-  if (isLoading && data.length === 0) return <Skeleton times={6} className="h-10 w-full"/>
-  if (error) return <div>{error.message}</div>
+  if (isLoadingAllUsers) return <Skeleton times={6} className="h-10 w-full"/>
+  if (isLoadingUsersError) return <div>{error.message}</div>;
 
   return (
     <div>
       <div className="flex flex-row justify-between m-3">
         <h1 className="m-2 text-xl">Users</h1>
-        <Button success onClick={handleUserAdd}>
-          + Add User
-        </Button>
+        { isCreatingUser ? 'Creating User...' :
+          <Button success onClick={handleUserAdd}>
+            + Add User
+          </Button>
+        }
+        { isCreatingUserError && 'error' }
       </div>
       {renderedUsers}
     </div>
--- client/src/media/store/thunks/addUser.js
+++ client/src/media/store/thunks/addUser.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import {faker} from '@faker-js/faker';
 
 export const addUser = createAsyncThunk("users/add", async (user) => {
-  const { data } = await axios.post(`http://localhost:3005/users`, {
+  const { data } = await axios.post(`htp://localhost:3005/users`, {
     name: faker.name.fullName()
 });
   return data;
